cdd5e0e9890e9358b6a28523c9a001f06b7f20e2
nicole-contreras <nicolecontreras9@icloud.com>
2025-12-19 15:45:37 -0500

    Authentication (Frontend)

---
+++ client/src/contexts/LoginContext.ts
@@ -0,0 +1,26 @@
+import { type SafeUserInfo } from "@gamenite/shared";
+import { createContext } from "react";
+import { type GameSocket } from "../util/types.ts";
+
+/**
+ * The user information held as part of a login context
+ *
+ * - user - the current user
+ * - pass - the user's password
+ * - reset - a callback that logs out the user
+ */
+export interface AuthContext {
+  user: SafeUserInfo;
+  pass: string;
+  reset: () => void;
+}
+
+/**
+ * See useLoginContext()
+ */
+export const LoginContext = createContext<
+  | (AuthContext & {
+      socket: GameSocket;
+    })
+  | null
+>(null);
