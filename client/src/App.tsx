f3d3fbcfbe838ded895b3a45aa1e5ff17e6099ec
nicole-contreras <nicolecontreras9@icloud.com>
2026-02-01 14:34:37 -0500

    fix app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,30 @@
 /* eslint no-console: "off" */
 
-import { Routes, Route, useLocation } from "react-router-dom";
+
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { useState } from "react";
 import Login from "./pages/Login.tsx";
 import type { AuthContext } from "./contexts/LoginContext.ts";
 import Layout from "./components/Layout.tsx";
 import Home from "./pages/Home.tsx";
-import GameList from "./pages/GameList.tsx";
-import NewGame from "./pages/NewGame.tsx";
-import Game from "./pages/Game.tsx";
+import ThreadList from "./pages/ThreadList.tsx";
+import Profile from "./pages/Profile.tsx";
 import { io } from "socket.io-client";
 import type { GameSocket } from "./util/types.ts";
 import LoggedInRoute from "./components/LoggedInRoute.tsx";
+import NewGame from "./pages/NewGame.tsx";
+import Game from "./pages/Game.tsx";
+import GameList from "./pages/GameList.tsx";
+import ThreadPage from "./pages/ThreadPage.tsx";
 import { ErrorBoundary } from "react-error-boundary";
 import fallback from "./fallback.tsx";
+import NewThread from "./pages/NewThread.tsx";
 import TimeContextKeeper from "./components/UpdatingTimeContext.tsx";
+import { GameListContextProvider } from "./contexts/GameListContext.tsx";
 
 /** If `true`, all incoming socket messages will be logged */
 const DEBUG_SOCKETS = false;
 
-/**
- * Websocket connection for the app. It would be natural to define this in a
- * useEffect hook, but the React docts advise against this.
- * https://react.dev/learn/you-might-not-need-an-effect#initializing-the-application
- * */
 let socket: GameSocket | null = null;
 if (typeof window !== "undefined") {
   socket = io();
@@ -39,30 +40,39 @@ function NoSuchRoute() {
   return `No page found for route '${pathname}'`;
 }
 
+
 export default function App() {
   const [auth, setAuth] = useState<AuthContext | null>(null);
   return (
     socket && (
-      <Routes>
-        <Route path="/login" element={<Login setAuth={(auth) => setAuth(auth)} />} />
-        <Route
-          element={
-            <LoggedInRoute auth={auth} socket={socket}>
-              <TimeContextKeeper updateFrequency={20 * 1000}>
-                <ErrorBoundary fallbackRender={fallback}>
-                  <Layout />
-                </ErrorBoundary>
-              </TimeContextKeeper>
-            </LoggedInRoute>
-          }
-        >
-          <Route path="/" element={<Home />} />
-          <Route path="/games" element={<GameList />} />
-          <Route path="/game/new" element={<NewGame />} />
-          <Route path="/game/:gameId" element={<Game />} />
-          <Route path="/*" element={<NoSuchRoute />} />
-        </Route>
-      </Routes>
+      <GameListContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login setAuth={(auth) => setAuth(auth)} />} />
+            <Route
+              element={
+                <LoggedInRoute auth={auth} socket={socket}>
+                  <TimeContextKeeper updateFrequency={20 * 1000}>
+                    <ErrorBoundary fallbackRender={fallback}>
+                      <Layout />
+                    </ErrorBoundary>
+                  </TimeContextKeeper>
+                </LoggedInRoute>
+              }
+            >
+              <Route path="/" element={<Home />} />
+              <Route path="/forum" element={<ThreadList />} />
+              <Route path="/forum/post/new" element={<NewThread />} />
+              <Route path="/forum/post/:threadId" element={<ThreadPage />} />
+              <Route path="/games" element={<GameList />} />
+              <Route path="/game/new" element={<NewGame />} />
+              <Route path="/game/:gameId" element={<Game />} />
+              <Route path="/profile/:username" element={<Profile />} />
+              <Route path="/*" element={<NoSuchRoute />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </GameListContextProvider>
     )
   );
 }
